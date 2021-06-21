09eb1695b6be4c7efdad126282496b45bc5b6e2b
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2021-06-21 15:33:20 +0700

    Initial Commit

---
+++ MC2-KisahKata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC2-KisahKata
+//
+//  Created by Dimas Putro on 17/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MC2-KisahKata' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MC2-KisahKata
+  pod 'ActiveLabel'
+
+end
