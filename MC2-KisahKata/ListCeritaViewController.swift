8c0788ab0251d05c95d2808547b8b21e771e61f7
Mulazi <Mulazihd@gmail.com>
2021-06-23 08:57:44 +0700

    Adding Img Array

--- MC2-KisahKata/ListCeritaViewController.swift
+++ MC2-KisahKata/ListCeritaViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ListCeritaViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var cerita = ["Legenda Danau Toba", "Lanj. Legenda Danau Toba", "Petani dan Angsa Bertelur Emas", "Lanj. Petani dan Angsa Bertelur Emas", "Legenda Sangkuriang", "Legenda Loro Jonggrang"]
+    var thumbnail = ["Candi Prambanan.img", "Danau Toba.img", "Danau Toba.img", "Danau Toba.img", "Danau Toba.img", "Danau Toba.img"]
 
     @IBOutlet weak var ceritaTableView: UITableView!
     
@@ -34,9 +35,10 @@ class ListCeritaViewController: UIViewController, UITableViewDelegate, UITableVi
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = ceritaTableView.dequeueReusableCell(withIdentifier: "ceritaCell") as! CeritaTableViewCell
         let story = cerita[indexPath.row]
+        let img = thumbnail[indexPath.row]
         
         cell.ceritaLabel.text = story
-        cell.ceritaImage.image = UIImage(named: story)
+        cell.ceritaImage.image = UIImage(named: img)
         
 //        cell.ceritaView.layer.cornerRadius = cell.ceritaView.frame.height / 2
 //        cell.ceritaImage.layer.cornerRadius = cell.ceritaImage.frame.height / 2
