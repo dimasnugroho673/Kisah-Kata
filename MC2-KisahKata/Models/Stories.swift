290b9bd7f892c624b47fd19fb9281028dbabe50d
Dimas Nugroho Putro <dimasnugroho673@gmail.com>
2021-06-24 20:37:01 +0700

    nambahin model stories untuk data

---
+++ MC2-KisahKata/Models/Stories.swift
@@ -0,0 +1,64 @@
+//
+//  Stories.swift
+//  MC2-KisahKata
+//
+//  Created by Dimas Putro on 24/06/21.
+//
+
+import Foundation
+
+class Stories: Identifiable {
+    // Properties
+    let id: Int
+    let title: String
+    let coverImage: String
+    let stories: [String]
+    let storyIlustrations: [String]
+    let highlightedWords: [String]
+    let videoDetail: String
+    let descriptionDetail: String
+    
+    init(id: Int, title: String, coverImage: String, stories: [String], storyIlustrations: [String], highlightedWords: [String], videoDetail: [String], descriptionDetail: [String]) {
+//        self.id = id
+//        self.title = title
+//        self.coverImage = coverImage
+//        self.stories = stories
+//        self.storyIlustrations = storyIlustrations
+//        self.highlightedWords = highlightedWords
+//        self.videoDetail = videoDetail
+//        self.descriptionDetail = descriptionDetail
+        
+        let stories: [[String]] = [
+            [
+                "Pada zaman dahulu",
+                "hiduplah seorang pemuda tani yatim piatu yang tampan bernama Toba",
+                "di bagian utara pulau Sumatra.",
+                "Daerah tersebut sangatlah kering",
+                "Pemuda itu hidup dari bertani",
+                "dan memancing ikan."
+            ],
+        ]
+        
+        let highlightedWords: [[String]] = [
+            [
+                "",
+                "yatim",
+                "tampan",
+                "",
+                "kering",
+                "bertani",
+                "memancing"
+            ]
+        ]
+        
+        
+        
+        Stories(id: 1, title: "Legenda Danau Toba", coverImage: "", stories: stories[0], storyIlustrations: [], highlightedWords: highlightedWords[0], videoDetail: [], descriptionDetail: <#T##[String]#>)
+    }
+    
+    
+
+    
+    
+    
+}
