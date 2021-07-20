e2d5f3fff4a5b61f66e3722132119356f755b2f8
Farendza <farendza.muda@gmail.com>
2021-07-20 23:45:13 +0700

    Halaman Home dan Data modelnya

---
+++ MC2-KisahKata/Cell/TemaModel.swift
@@ -0,0 +1,15 @@
+//
+//  TemaModel.swift
+//  MC2-KisahKata
+//
+//  Created by Farendza Muda on 20/07/21.
+//
+
+import Foundation
+
+struct TemaModel {
+    var Urutan: String
+    var Tema: String
+    var ImageTema: String
+    var ListJudul: [JudulModel]
+}
